59a286058cfd3a582675f5cfd7928f84ef008a87
MahamudulHasan-Sami <thisismahamudulhasan@gmail.com>
2026-03-06 23:14:24 +0600

    login page has been designed and made interactive

--- script/login.js
+++ script/login.js
@@ -1,1 +1,13 @@
 
+const usernameInputId = document.getElementById("usernameInputId");
+const passwordInputId = document.getElementById("passwordInputId");
+const loginBtnId = document.getElementById("loginBtnId");
+
+loginBtnId.addEventListener("click", ()=>{
+    if(usernameInputId.value == 'admin' && passwordInputId.value == 'admin123'){
+        window.location.assign("./home.html");
+    }else{
+        alert("Login Failed");
+        return;
+    }
+})
